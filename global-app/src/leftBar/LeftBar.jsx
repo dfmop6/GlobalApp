7e7451f8e6960a44d54d6196061a2d6e54cbdc34
dfmop6 <ss.elmachehour@gmail.com>
2024-10-01 17:09:48 +0100

    rebuild the project and set up the components

---
+++ global-app/src/leftBar/LeftBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default function LeftBar() {
+  return (
+    <div className='bg-black h-screen w-[250px] text-white'>
+        <div className='mx-auto pt-2'>
+            <h1 class="logo__text text-3xl font-extrabold text-gray-900 dark:text-white"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Is</span>sam.</h1>
+            <div className='logo__line p-[2px] w-[35px] mx-[80px] bg-white'></div>
+        </div>
+        <div className=''></div>
+    </div>
+  )
+}
--- global-app/tailwind.config.js
+++ global-app/tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [],
-}
+};
